bd21ab05938d8755b5a8f451fd992b176ce2a6d5
Goutam-11 <goutamkumar.sharmq@gmail.com>
2025-10-26 13:57:57 +0530

    created a tRpc security layer for the type safe api calls.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 import { Button } from "@/components/ui/button";
-import prisma from "@/lib/db"
+import { getQueryClient, trpc } from "@/trpc/server";
+import { Client } from "./client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+
 const Page = async() => {
-  const data = await prisma.user.findMany();
+  const queryClinet = getQueryClient();
+  void queryClinet.prefetchQuery(trpc.getUsers.queryOptions())
   return (
     <div className="text-red-700 font-bold">
       <h1>Hello, World!</h1>
-      {JSON.stringify(data)}
+      <HydrationBoundary state={dehydrate(queryClinet)}>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Client />
+        </Suspense>
+      </HydrationBoundary>
       <Button variant="outline">Click me</Button>
     </div>
   );
