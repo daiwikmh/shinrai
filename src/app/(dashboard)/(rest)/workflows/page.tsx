0d9af525eef1b67928bf1f16e6ca568bd3bc8505
Goutam-11 <goutamkumar.sharmq@gmail.com>
2025-10-30 16:12:02 +0530

    Updated sidebar

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,12 +1,25 @@
-import { requireAuth } from "@/lib/auth-utils";
+"use client";
+import { Button } from "@/components/ui/button";
+// import { requireAuth } from "@/lib/auth-utils";
+import { useTRPC } from "@/trpc/client";
 
-const Page = async () => {
-  await requireAuth();
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+const Page = () => {
+  // await requireAuth();
+  const trpc = useTRPC();
+  const testAi = useMutation(trpc.testAI.mutationOptions({
+    onSuccess: () => {
+      toast.info("AI job queued");
+    }
+  }));
   
   return (
     <div>
       <h1>Workflows</h1>
       <p>Manage your workflows here.</p>
+      <Button disabled={testAi.isPending} onClick={() => testAi.mutate()}>Test aI</Button>
     </div>
   );
 };
