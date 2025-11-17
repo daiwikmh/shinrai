163810e33e288ed89e68fa5655601ef7b80307b8
Goutam-11 <goutamkumar.sharmq@gmail.com>
2025-11-17 23:29:00 +0530

    ui correction

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -5,23 +5,14 @@ import { SuiProvider } from "@/providers/sui-provider";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-<<<<<<< HEAD
    <SuiProvider>
-     <SidebarProvider className="flex min-h-screen w-full">
+     <SidebarProvider className="flex max-h-screen w-full bg-sidebar">
        <AppSidebar/>
-       <SidebarInset className="bg-background flex-1 flex flex-col overflow-hidden">
+       <SidebarInset className="bg-background flex-1 flex flex-col overflow-hidden rounded-2xl m-2 border-2 border-accent-foreground">
          {children}
        </SidebarInset>
      </SidebarProvider>
    </SuiProvider>
-=======
-   <SidebarProvider className="bg-sidebar">
-     <AppSidebar/>
-     <SidebarInset className="bg-background rounded-2xl m-3 border border-accent-foreground">
-       {children}
-     </SidebarInset>
-   </SidebarProvider>
->>>>>>> a104939eb85510118843b94bf61051aaba3597e2
   )
 }
 export default Layout;
