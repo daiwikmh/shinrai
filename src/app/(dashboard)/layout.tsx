ee8d6a2900f2e5811e2c400415d11cd1db96aa1d
daiwikmh <daiwikdomain@gmail.com>
2025-11-17 23:31:26 +0530

    dont know

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -5,23 +5,12 @@ import { SuiProvider } from "@/providers/sui-provider";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-<<<<<<< HEAD
-   <SuiProvider>
-     <SidebarProvider className="flex min-h-screen w-full">
-       <AppSidebar/>
-       <SidebarInset className="bg-background flex-1 flex flex-col overflow-hidden">
-         {children}
-       </SidebarInset>
-     </SidebarProvider>
-   </SuiProvider>
-=======
    <SidebarProvider className="bg-sidebar">
      <AppSidebar/>
      <SidebarInset className="bg-background rounded-2xl m-3 border border-accent-foreground">
        {children}
      </SidebarInset>
    </SidebarProvider>
->>>>>>> a104939eb85510118843b94bf61051aaba3597e2
   )
 }
 export default Layout;
