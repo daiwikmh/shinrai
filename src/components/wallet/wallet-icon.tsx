2c7b0da727b4e17d35c8f0ac839958f7c019ce6c
daiwikmh <daiwikdomain@gmail.com>
2025-11-26 22:51:07 +0530

    s

--- src/components/wallet/wallet-icon.tsx
+++ src/components/wallet/wallet-icon.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import { WalletStandardAdapterProvider } from "@mysten/wallet-standard";
 import Image from "next/image";
 import { WalletIcon } from "lucide-react";
 
-interface WalletIconComponentProps {
-  wallet: WalletStandardAdapterProvider;
+type WalletIconComponentProps = {
+  wallet: {
+    name: string;
+    icon?: string;
+  };
   className?: string;
-}
+};
 
 export function WalletIconComponent({ wallet, className = "h-6 w-6" }: WalletIconComponentProps) {
   if (wallet.icon) {
